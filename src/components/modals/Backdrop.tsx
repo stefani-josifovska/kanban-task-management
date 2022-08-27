e9dac8c39dba79d4ed93ba2e03f5d8942698a2ea
stefijosi <stefani.josifovska99@gmail.com>
2022-08-27 18:43:21 +0200

    backdrop height fixed

--- src/components/modals/Backdrop.tsx
+++ src/components/modals/Backdrop.tsx
@@ -1,6 +1,6 @@
 const Backdrop: React.FC<{ onClick: () => void }> = ({ onClick }) => {
   return (
-    <div onClick={onClick} className="mt-16 fixed top-0 left-0 w-full h-[calc(100vh-90px)] z-20 bg-black/75 h-[calc(100vh-4rem)]" />
+    <div onClick={onClick} className="mt-16 fixed top-0 left-0 w-full z-20 bg-black/75 h-[calc(100vh-4rem)]" />
   );
 };
 
